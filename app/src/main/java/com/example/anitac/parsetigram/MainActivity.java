635482354adfc03c4f428c699a33590d84f0a3af
Annamary Cartagena <anitac@fb.com>
2018-07-09 15:25:14 -0700

    Initial commit, decorated activity, 2/5 videos finished. server not implemented

---
+++ app/src/main/java/com/example/anitac/parsetigram/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.anitac.parsetigram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
