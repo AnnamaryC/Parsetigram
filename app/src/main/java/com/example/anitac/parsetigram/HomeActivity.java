ce7bfa0e5486572cc9f31bc00f1e9984d2a9002d
Annamary Cartagena <anitac@fb.com>
2018-07-10 14:36:04 -0700

    video 4/5 done

--- app/src/main/java/com/example/anitac/parsetigram/HomeActivity.java
+++ app/src/main/java/com/example/anitac/parsetigram/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.example.anitac.parsetigram;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+
+import com.example.anitac.parsetigram.Models.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,15 +16,26 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop()
+                .withUser();
+
+
+
+        postQuery.findInBackground(new FindCallback<Post>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void done(List<Post> objects, ParseException e) {
+                if (e== null){
+                    for(int i = 0; i < objects.size(); i++){
+                        Log.d("HomeActivity", "Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername());
+                    }
+
+                } else{
+                    e.printStackTrace();
+                }
             }
         });
     }
