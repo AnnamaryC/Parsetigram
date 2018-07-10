cca34da69ab2dd9920560aef849bb159c8f13fd8
Annamary Cartagena <anitac@fb.com>
2018-07-09 17:46:53 -0700

    added finish() to Login(str, str)

--- app/src/main/java/com/example/anitac/parsetigram/MainActivity.java
+++ app/src/main/java/com/example/anitac/parsetigram/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.anitac.parsetigram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +19,40 @@ public class MainActivity extends AppCompatActivity {
     private Button mLogin;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //Bundle SaveInstanteState saves what u haave wrtten down when avtivity is destroyed i.e. orientation
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUsername = findViewById(R.id.vUsername);
+        mPassword = findViewById(R.id.vPass);
+        mLogin = findViewById(R.id.vLogin);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = mUsername.getText().toString();
+                final String password = mPassword.getText().toString();
+
+                Login(username, password);
+            }
+        });
     }
+
+    private void Login(String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null){
+                    Log.d("LogInActivity", "Log in successful!");
+                    final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Log.e("LogInActivity", "Log in failure.");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 }
